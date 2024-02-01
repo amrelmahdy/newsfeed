8ad2f30f1bb3f62efc853ca0ca840845938de0ee
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-01 02:25:46 +0200

    feat: add basic bottom tabs navigation

--- App.tsx
+++ App.tsx
@@ -1,7 +1,10 @@
 import * as React from 'react';
 import { View, Text } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
-import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+const Tab = createBottomTabNavigator();
 
 function HomeScreen() {
   return (
@@ -11,15 +14,23 @@ function HomeScreen() {
   );
 }
 
-const Stack = createNativeStackNavigator();
+function SettingsScreen() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Settings Screen</Text>
+    </View>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen name="Home" component={HomeScreen} />
-      </Stack.Navigator>
+      <Tab.Navigator>
+        <Tab.Screen name="Home" component={HomeScreen} />
+        <Tab.Screen name="Settings" component={SettingsScreen} />
+      </Tab.Navigator>
     </NavigationContainer>
+
   );
 }
 
