8ae293244ce0d4fde322d2f2a96deda067796a12
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-01 20:15:22 +0200

    feat: add vector icons and setup icons for settings screen

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { View, FlatList, Text, StyleSheet, TouchableOpacity } from 'react-native';
+import Icon from 'react-native-vector-icons/FontAwesome';
 
 const SettingsScreen = ({ navigation }) => {
   const data = [
-    { screenName: 'AppearanceScreen', name: 'Appearance' },
-    { screenName: 'LanguageScreen', name: 'Language' },
+    { screenName: 'AppearanceScreen', name: 'Appearance', icon: 'moon-o', value: 'Systemdd Default' },
+    { screenName: 'LanguageScreen', name: 'Language', icon: 'language', value: 'en' },
   ];
 
   // Function to render each item
@@ -12,13 +13,18 @@ const SettingsScreen = ({ navigation }) => {
     <TouchableOpacity onPress={() => {
       navigation.navigate(item.screenName, {});
     }} style={{ padding: 10, flexDirection: 'row', justifyContent: 'space-between' }}>
-      <Text>{item.name}</Text>
-      <Text>System Default</Text>
+      <View style={{ flexDirection: 'row' }}>
+        <Icon name={item.icon} size={20} style={{ marginRight: 5 }} />
+        <Text>{item.name}</Text>
+      </View>
+      <Text style={{ color: '#888' }}>{ item.value }</Text>
     </TouchableOpacity>
   );
 
   return (
     <View style={styles.container}>
+
+
       <FlatList
         data={data}
         renderItem={renderItem}
