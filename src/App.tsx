d4af1e2def3e6ef5ecf20ec0fd3d6f27c7bdfd65
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-01 11:33:32 +0200

    fix: types issues with details screen

--- src/App.tsx
+++ src/App.tsx
@@ -3,18 +3,49 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import NewScreen from './screens/News/News';
 import SettingsScreen from './screens/Settings/Settings';
+import NewsDetailsScreen from './screens/NewsDetails/NewsDetails';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+export type RootStackParamList = {
+  News: undefined;
+  Settings: undefined
+  NewsDetails: {
+    urlToImage: string,
+    title: string, 
+    description: string, 
+    content: string
+  };
+};
+
 
 const Tab = createBottomTabNavigator();
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+function NewsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="News" component={NewScreen} />
+      <Stack.Screen name="NewsDetails" component={NewsDetailsScreen} />
+    </Stack.Navigator>
+  );
+}
+
+function SettingsStack() {
+  return (
+    <Stack.Navigator>
+      <Stack.Screen name="Settings" component={SettingsScreen} />
+    </Stack.Navigator>
+  );
+}
 
 function App() {
   return (
     <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="News" component={NewScreen} />
-        <Tab.Screen name="Settings" component={SettingsScreen} />
+      <Tab.Navigator screenOptions={{ headerShown: false }}>
+        <Tab.Screen name="News" component={NewsStack} />
+        <Tab.Screen name="Settings" component={SettingsStack} />
       </Tab.Navigator>
     </NavigationContainer>
-
   );
 }
 
