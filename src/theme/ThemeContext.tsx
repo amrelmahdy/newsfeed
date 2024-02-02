784ba09c9c95c6a926be9661378937bc69b4070b
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-02 18:46:14 +0200

    feat: create theme context

---
+++ src/theme/ThemeContext.tsx
@@ -0,0 +1,61 @@
+import React, { createContext, useState, useEffect, ReactNode } from 'react';
+import { useColorScheme } from 'react-native';
+import { get, save } from '../storage';
+
+
+interface ThemeContextType {
+  theme: string;
+  toggleTheme: (newTheme: string) => void;
+  useSystemTheme: () => void;
+}
+
+const ThemeContext = createContext<ThemeContextType>({
+  theme: 'light',
+  toggleTheme: () => { },
+  useSystemTheme: () => { }
+});
+
+interface ThemeProviderProps {
+  children: ReactNode;
+}
+
+export const ThemeProvider = ({ children }: ThemeProviderProps) => {
+  const colorScheme: string = useColorScheme() || 'light';
+  const [theme, setTheme] = useState<string>(colorScheme || 'light');
+  useEffect(() => {
+    const getTheme = async () => {
+      try {
+        const savedTheme: string | null = await get('Theme');
+        if (savedTheme) {
+          setTheme(savedTheme);
+        }
+      } catch (error) {
+        console.log('Error loading theme:', error);
+      }
+    };
+    getTheme();
+  }, []);
+
+  useEffect(() => {
+    if (colorScheme) {
+      setTheme(colorScheme);
+    }
+  }, [colorScheme]);
+
+  const toggleTheme = (newTheme: string) => {
+    setTheme(newTheme);
+    save('Theme', newTheme); // Save selected theme to storage
+  };
+
+  const useSystemTheme = () => {
+    setTheme(colorScheme);
+    save('Theme', colorScheme);
+  };
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme, useSystemTheme }}>
+      {children}
+    </ThemeContext.Provider>
+  );
+};
+export default ThemeContext;
