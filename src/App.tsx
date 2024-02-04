02565f765ae2ee70844f31100913df0d8e3b1dc2
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-04 12:00:37 +0200

    fix: restructure app navigation

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,29 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navigation from './navigation';
-
 import { ThemeProvider } from './theme/ThemeContext';
+import { useTranslation, I18nextProvider } from 'react-i18next';
+import { get } from './storage';
+
+
 
 function App() {
+  const { i18n } = useTranslation()
+  useEffect(() => {
+    getLanguage();
+  }, [])
+
+  const getLanguage = async () => {
+    const lang: string | undefined = await get('Language') || 'en';
+    i18n.changeLanguage(lang);
+  }
+
   return (
-    <ThemeProvider>
-      <Navigation />
-    </ThemeProvider>
+    <I18nextProvider i18n={i18n}>
+      <ThemeProvider>
+        <Navigation />
+      </ThemeProvider>
+    </I18nextProvider>
+
   );
 }
 
--- src/navigation/index.tsx
+++ src/navigation/index.tsx
@@ -11,8 +11,13 @@ import { Appearance } from 'react-native';
 import ThemeContext from '../theme/ThemeContext';
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import { ThemeKey, themes } from '../theme/colors';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import AppearanceScreen from '../screens/Settings/Appearance/Appearance';
+import LanguageScreen from '../screens/Settings/Language/Language';
+import { useTranslation } from 'react-i18next';
 
 export type RootStackParamList = {
+    Home: undefined;
     NewsScreen: undefined;
     SettingsScreen: undefined;
     Settings: undefined
@@ -26,13 +31,20 @@ export type RootStackParamList = {
     };
 };
 
+const Stack = createNativeStackNavigator<RootStackParamList>();
+
+
 function Navigation() {
     const { theme } = useContext(ThemeContext);
+    const { t } = useTranslation();
     return (
-        <NavigationContainer theme={themes[theme]}>
-            <BottomTabNavigation />
+        <NavigationContainer theme={themes[theme]} >
+            <Stack.Navigator screenOptions={{ headerShown: false, headerTintColor: themes[theme].colors.primary }}>
+                <Stack.Screen name="Home" component={BottomTabNavigation} />
+                <Stack.Screen name="AppearanceScreen" component={AppearanceScreen} options={{ title: t('appearance'), headerShown: true, headerBackTitleVisible: false}} />
+                <Stack.Screen name="LanguageScreen" component={LanguageScreen} options={{ title: t("lang"), headerShown: true, headerBackTitleVisible: false }} />
+            </Stack.Navigator>
         </NavigationContainer>
-
     );
 }
 
