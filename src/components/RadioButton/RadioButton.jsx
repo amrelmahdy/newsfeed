c1343352f492b805a093a794b0e5235f04ea534e
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-05 16:12:10 +0200

    feat: write RadioButton unit testing

--- src/components/RadioButton/RadioButton.jsx
+++ src/components/RadioButton/RadioButton.jsx
@@ -14,17 +14,18 @@ const RadioButton = ({ data, defaultValue, onSelectButton }) => {
 	return (
 		<View>
 			{data.map(res => {
-				// console.log(value , res.key, defaultValue)
 				return (
 					<View key={res.key} style={styles.container}>
-						<Text style={styles.radioText}>{res.text}</Text>
+						<Text style={styles.radioText} testID={`radio-text-${res.key}`}>{res.text}</Text>
 						<TouchableOpacity
 							style={styles.radioCircle}
 							onPress={() => {
 								onSelectButton?.(res.key)
 								setValue(res.key);
-							}}>
-                            {value === res.key && <View style={styles.selectedRb} />}
+							}}
+							testID={`radio-button-${res.key}`}>
+							{value === res.key && <View style={styles.selectedRb} testID={`selected-radio-${res.key}`}
+							/>}
 						</TouchableOpacity>
 					</View>
 				);
@@ -39,16 +40,16 @@ export default RadioButton;
 
 const styling = colors => StyleSheet.create({
 	container: {
-        marginBottom: 15,
-        alignItems: 'center',
-        flexDirection: 'row',
+		marginBottom: 15,
+		alignItems: 'center',
+		flexDirection: 'row',
 		justifyContent: 'space-between',
 	},
-    radioText: {
-        marginRight: 35,
-        fontSize: 15,
-        color: colors.textColor,
-    },
+	radioText: {
+		marginRight: 35,
+		fontSize: 15,
+		color: colors.textColor,
+	},
 	radioCircle: {
 		height: 20,
 		width: 20,
@@ -63,6 +64,6 @@ const styling = colors => StyleSheet.create({
 		height: 15,
 		borderRadius: 50,
 		backgroundColor: colors.primary,
-    },
+	},
 
 });
