e76a1628da8ed16500688bd7e7513fbcb4294a23
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-09 02:05:37 +0200

    fix: refactor BottomTabNavBar

--- src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
+++ src/navigation/BottomTabNavigation/BottomTabNavigation.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {  createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import NewsScreen from '../../screens/News/News';
 import SettingsScreen from '../../screens/Settings/Settings';
 import Icon from 'react-native-vector-icons/Ionicons';
@@ -14,22 +14,17 @@ const Tab = createBottomTabNavigator<BottomTabParamList>();
 
 function BottomTabNavigation() {
   const { t } = useTranslation();
-
-  const tabs = [
-    { screenName: t("news"), screenComponent: NewsScreen, iconName: 'newspaper-outline' },
-    { screenName: t('settings'), screenComponent: SettingsScreen, iconName: 'settings' },
-  ];
-
   return (
     <Tab.Navigator screenOptions={{ headerShown: true }}>
       <Tab.Screen
         name='NewsScreen'
         component={NewsScreen}
         options={{
+          tabBarTestID: 'tab_bar_news_tab',
           unmountOnBlur: true,
           title: t("news"),
           tabBarIcon: ({ color, size }) => (
-            <Icon name='newspaper-outline' size={size} color={color} />
+            <Icon testID='tab_bar_news_tab_icon' name='newspaper-outline' size={size} color={color} />
           ),
         }}
       />
