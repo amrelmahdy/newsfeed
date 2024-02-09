905aa5a049cae91053ef41eb98283dc171be2f20
Amr El Mahdy <amr.elmahdy@vodafone.com>
2024-02-09 02:20:03 +0200

    fix: remove other languages

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -1,13 +1,10 @@
 import i18n from 'i18next';
 import { initReactI18next } from 'react-i18next';
 import en from './languages/en';
-import fr from './languages/fr';
-import de from './languages/de';
-import es from './languages/es';
 import it from './languages/it';
 
 const resources = { 
-    en, fr, de, es, it
+    en, it
 };
 i18n.use(initReactI18next) 
     .init({
